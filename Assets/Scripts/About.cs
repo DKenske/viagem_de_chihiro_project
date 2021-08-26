ff19043d675406c42f7d979e1b4cb30171f5ba50
Herefrid <animesv10@gmail.com>
2021-08-25 21:09:46 -0300

    first commit

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class About : MonoBehaviour
+{
+    public GameObject credits;
+    private int yes=0;
+
+
+    private void Update()
+    {
+        QuitAbout();
+    }
+    public void AboutGame()
+    {
+        if (yes == 0)
+        {
+            credits.SetActive(true);
+            yes = 1;
+        }
+    }
+
+    public void QuitAbout()
+    {
+        if (yes == 1 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            credits.SetActive(false);
+            yes = 0;
+        }
+    }
+}
